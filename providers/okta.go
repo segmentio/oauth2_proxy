01c9e89fbe629a6ddbb23f9e6f5663da60343c08
Eric Ellett <eric.ellett@segment.com>
2018-06-26 15:47:19 -0700

    Added refresh flow for Okta

--- providers/okta.go
+++ providers/okta.go
@@ -1,10 +1,15 @@
 package providers
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
+	"time"
 
 	"github.com/bitly/oauth2_proxy/api"
 )
@@ -16,7 +21,7 @@ type OktaProvider struct {
 func NewOktaProvider(p *ProviderData) *OktaProvider {
 	p.ProviderName = "Okta"
 	if p.Scope == "" {
-		p.Scope = "openid profile email"
+		p.Scope = "openid profile email offline_access"
 	}
 	return &OktaProvider{ProviderData: p}
 }
@@ -43,6 +48,7 @@ func (p *OktaProvider) SetOktaDomain(domain string) {
 			Path:   "/oauth2/v1/userinfo",
 		}
 	}
+
 }
 
 func getOktaHeader(access_token string) http.Header {
@@ -85,4 +91,124 @@ func (p *OktaProvider) GetUserName(s *SessionState) (string, error) {
 
 func (p *OktaProvider) ValidateSessionState(s *SessionState) bool {
 	return validateToken(p, s.AccessToken, getOktaHeader(s.AccessToken))
+}
+
+func (p *OktaProvider) Redeem(redirectURL, code string) (s *SessionState, err error) {
+	if code == "" {
+		err = errors.New("missing code")
+		return
+	}
+
+	params := url.Values{}
+	params.Add("redirect_uri", redirectURL)
+	params.Add("client_id", p.ClientID)
+	params.Add("client_secret", p.ClientSecret)
+	params.Add("code", code)
+	params.Add("grant_type", "authorization_code")
+	var req *http.Request
+	req, err = http.NewRequest("POST", p.RedeemURL.String(), bytes.NewBufferString(params.Encode()))
+	if err != nil {
+		return
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return
+	}
+	var body []byte
+	body, err = ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return
+	}
+
+	if resp.StatusCode != 200 {
+		err = fmt.Errorf("got %d from %q %s", resp.StatusCode, p.RedeemURL.String(), body)
+		return
+	}
+
+	log.Printf("BODY custom redeem: %s", body)
+
+	var jsonResponse struct {
+		AccessToken  string `json:"access_token"`
+		RefreshToken string `json:"refresh_token"`
+		ExpiresIn    int64  `json:"expires_in"`
+		IdToken      string `json:"id_token"`
+	}
+	err = json.Unmarshal(body, &jsonResponse)
+	if err != nil {
+		return
+	}
+	s = &SessionState{
+		AccessToken:  jsonResponse.AccessToken,
+		ExpiresOn:    time.Now().Add(time.Duration(jsonResponse.ExpiresIn) * time.Second).Truncate(time.Second),
+		RefreshToken: jsonResponse.RefreshToken,
+	}
+	return
+}
+
+func (p *OktaProvider) RefreshSessionIfNeeded(s *SessionState) (bool, error) {
+	//@todo: Remove from here before accepting PR. This is for local testing
+	if s == nil || s.ExpiresOn.After(time.Now().Add(time.Minute*59).Add(time.Second*50)) || s.RefreshToken == "" {
+		return false, nil
+	}
+
+	newToken, duration, err := p.redeemRefreshToken(s.RefreshToken)
+	if err != nil {
+		return false, err
+	}
+
+	// re-check that the user is in the proper google group(s)
+	if !p.ValidateGroup(s.Email) {
+		return false, fmt.Errorf("%s is no longer in the group(s)", s.Email)
+	}
+
+	origExpiration := s.ExpiresOn
+	s.AccessToken = newToken
+	s.ExpiresOn = time.Now().Add(duration).Truncate(time.Second)
+	log.Printf("refreshed access token %s (expired on %s)", s, origExpiration)
+	return true, nil
+}
+
+func (p *OktaProvider) redeemRefreshToken(refreshToken string) (token string, expires time.Duration, err error) {
+	params := url.Values{}
+	params.Add("client_id", p.ClientID)
+	params.Add("client_secret", p.ClientSecret)
+	params.Add("refresh_token", refreshToken)
+	params.Add("grant_type", "refresh_token")
+	var req *http.Request
+	req, err = http.NewRequest("POST", p.RedeemURL.String(), bytes.NewBufferString(params.Encode()))
+	if err != nil {
+		return
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return
+	}
+	var body []byte
+	body, err = ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return
+	}
+
+	if resp.StatusCode != 200 {
+		err = fmt.Errorf("got %d from %q %s", resp.StatusCode, p.RedeemURL.String(), body)
+		return
+	}
+
+	var data struct {
+		AccessToken string `json:"access_token"`
+		ExpiresIn   int64  `json:"expires_in"`
+	}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return
+	}
+	token = data.AccessToken
+	expires = time.Duration(data.ExpiresIn) * time.Second
+	return
 }
